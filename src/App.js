5eb64ae9827fdf06cab63de0005fc351b6dd37b7
noelkn18 <knobi@sbcglobal.net>
2020-11-11 21:28:05 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
-import {MenuItem, FormControl, Select} from "@material-ui/core";
-import './App.css';
+import {MenuItem, FormControl, Select, Card, CardContent} from "@material-ui/core";
 import InfoBox from "./InfoBox";
+import Map from "./Map";
+import './App.css';
+
 
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setInputCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -29,10 +33,25 @@ function App() {
   const onCountryChange = async (event) => {
     const countryCode = event.target.value; 
     setInputCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+        setInputCountry(countryCode);
+        setCountryInfo(data);
+        
+      });
   };
+
+  console.log ('COUNTRY INFO', countryInfo)
 
   return (
     <div className="app"> 
+      <div className="app__left">
       <div className="app__header">
         <h1>COVID-19 Tracker</h1>
         <FormControl className="app__dropdown">
@@ -44,21 +63,34 @@ function App() {
               ))}
           </Select>
         </FormControl>
-        </div>
-            
+      </div>
           <div className="app__stats">
           
-          <InfoBox title="Coronavirus Cases" />
+          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases}
+            total={countryInfo.cases} />
           
-          <InfoBox title="Recovered" />
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered} />
           
-          <InfoBox title="Deaths" />
-          </div>
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}/>
+      </div>
+        
+         <Map />
+
+  </div>
+    <Card className="app__right">
+      <CardContent>
+        <h3>Live Cases by Country</h3>
+
+        <h3>Worldwide new cases</h3>
 
 
+      </CardContent>
 
 
-    </div>
+    </Card> 
+  </div>
   );
 };
 
